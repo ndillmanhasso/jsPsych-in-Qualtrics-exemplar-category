125b002a0cb97547198ff7d70989fb16894b3ac4
Naseem Dillman-Hasso <40174188+ndillmanhasso@users.noreply.github.com>
2022-05-19 07:52:47 -0400

    typo fix

--- flanker/flanker_main.js
+++ flanker/flanker_main.js
@@ -15,7 +15,7 @@ var welcome = {
 var instructions = {
     type: "html-keyboard-response",
     stimulus: "<p>Below you see an item from a grocery store.</p>" +
-        "<img src='" + repo_site + "img/inc1.png'></img>" +
+        "<img src='" + repo_site + "img/plastic1.png'></img>" +
         "<p>Press the \"e\" key if the item contains plastic. </p>" +
         "<p>Press the \"i\" key if the item does not contain plastic. </p>" +
         "<p>Press any key to begin.</p>",
